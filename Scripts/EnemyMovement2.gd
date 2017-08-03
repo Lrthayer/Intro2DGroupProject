46ffc8151d84c7c82633dd0aeac37f9204bd3df7
lrthayer <lrthayer@iupui.edu>
2017-08-02 20:43:24 -0400

    added move and hide to mover

--- Scripts/EnemyMovement2.gd
+++ Scripts/EnemyMovement2.gd
@@ -37,7 +37,8 @@ func _ready():
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
-	
+	if self.get_parent().clicked:
+		self.set_global_pos(get_global_mouse_pos())
 	right_boundary = get_parent().get_child(1).get_child(0).get_global_pos().x - 27
 	left_boundary = get_parent().get_child(2).get_child(0).get_global_pos().x + 27
 	
@@ -61,3 +62,6 @@ func _fixed_process(delta):
 		move(Vector2(-1,0))
 		
 	
+
+func _on_mouse_enter():
+	pass # replace with function body

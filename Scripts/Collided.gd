a8a5fe5d9418d5f89969be2027f2a49470c7f4d5
lincoln <linkthayer@sbcglobal.net>
2018-02-13 16:53:47 -0500

    checkpoint
    adding settings to each object

--- Scripts/Collided.gd
+++ Scripts/Collided.gd
@@ -1,21 +1,17 @@
 extends StaticBody2D
 
-
-
-#Class variables
-var hp = 3
-
 #Turret is colliding with another object
 func collided(dmg = 0):
 	
-	hp -= dmg
+	#hp -= dmg
+	self.get_parent().get_parent().health -= self.get_parent().get_parent().damage
 	GLOBALS.g_current_score += 10
 	
 	#set health
-	get_node("Hp").set_value(hp)
+	get_node("Hp").set_value(self.get_parent().get_parent().health)
 	
 	#destroy object when health is zero
-	if (hp <= 0):
+	if (get_parent().get_parent().health <= 0):
 		GLOBALS.g_current_score += 10
 		GLOBALS.enemy_counter -= 1
 		get_parent().queue_free()

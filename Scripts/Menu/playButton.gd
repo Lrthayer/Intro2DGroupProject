bb6ccd8acf0849363484be028ae1f6f9c4b3f27d
Anthony Golubski-Allen <adgolubs@umail.iu.edu>
2020-01-28 21:31:34 -0500

    Added LoadPlaylist Scene
    User can press play and go to the load playlist page. I manage to break it. I go from start to level editor. Then press load button to load a new scene and press escape key, it creates an error.

--- Scripts/Menu/playButton.gd
+++ Scripts/Menu/playButton.gd
@@ -5,5 +5,7 @@ func _ready():
 
 #when pressed, user gets to pick which playlist to play
 func _pressed():
+	GLOBALS.last_scene = "start"
+	get_tree().change_scene("res://Scenes/Load_Interface/LoadPlaylist.tscn")
 	GLOBALS.reset()
 	#todo open playlist options

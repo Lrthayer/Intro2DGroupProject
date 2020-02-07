7766d3e5f1fbff10422bd9a978dc8e16bab56fdd
Lincoln <linkthayer@sbcglobal.net>
2020-02-06 23:16:49 -0500

    added power up to Attacker
    goes transparent 50% and has a "special power" bar to allows for visual representation of duration and recovery of said power

--- Scripts/Shoot.gd
+++ Scripts/Shoot.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 # class member variables go here, for example:
 var firePressed = false
 var laserObject = preload("res://Scenes/Laser.tscn")
@@ -14,6 +13,14 @@ var fireRateTimer = 0
 
 var bulletHeight = 1
 var bulletWidth = 1
+
+onready var specialBar = self.get_node("KinematicBody2D/special_bar")
+var specialButton=false
+var specialDuration= 3
+var currentspecialDuration = specialDuration
+export var specialTime = 5
+var specialTemp = specialTime
+var inSpecial = false
 
 #starts when loaded up in scene
 func _ready():
@@ -41,6 +48,12 @@ func _ready():
 		fireRateTimer = .5
 	else:
 		fireRateTimer = 0
+		
+		
+	#set special bar's max value to specialDuration
+	specialBar.max_value = specialTime
+	#set special Temp higher than speical Time so player can use special initially
+	specialTemp = specialTime + 1
 	set_process(true)
 
 #calls every frame
@@ -51,8 +64,10 @@ func _process(delta):
 	#Determine what button should be pressed for firing laser
 	if (GLOBALS.g_current_attacker == "player1"):
 		fireButton = Input.is_action_pressed("space_fire")
+		specialButton = Input.is_action_pressed("specialPower")
 	else:
 		fireButton = Input.is_action_pressed("right_fire")
+		specialButton = Input.is_action_pressed("specialPower2")
 	
 	#Player is shooting laser
 	if fireButton and !firePressed and fireRateTimer <= 0:
@@ -83,8 +98,36 @@ func _process(delta):
 		#set the position of the laser copy
 		laserInstance.global_position = get_node("KinematicBody2D/ShipSprite").global_position + Vector2(0,24)
 		
+	#if attacker is not using it's special and it's special isn't charged, charge special
+	if !inSpecial and specialBar.value < specialBar.max_value:
+		specialBar.value += delta
+		specialTemp += delta
+	#if user pressed the special power button make the sprite 50 percent opaque and turn off collisions
+	if specialButton and specialTemp > specialTime:
+		inSpecial = true
+		specialTemp = 0
+		self.get_node("KinematicBody2D/ShipSprite").modulate = Color(1,1,1,0.5)
+		
+	if inSpecial:
+		specialBar.max_value = specialDuration
+		currentspecialDuration -= delta
+		#decrease special bar
+		specialBar.value = (specialBar.value - delta)
+		if currentspecialDuration <= 0:
+			self.get_node("KinematicBody2D/ShipSprite").modulate = Color(1,1,1,1)
+			currentspecialDuration = specialDuration
+			#reset spceialBar
+			#specialBar.value = specialDuration
+			inSpecial = false
+	#if not in special tell special bar to use the recharge time as it's new max value
+	else:
+		specialBar.max_value = specialTime
+		
+	#print ("special Time : ", specialTime, " specialBar : " ,specialBar.value) 
+		
 	#Check to see if user pressed the spacebar or Numpad 0
 	firePressed = Input.is_action_pressed("space_fire") || Input.is_action_pressed("right_fire") 
+	specialButton = false
 
 func _on_DamageSpinBox_value_changed( value ):
 	damage = value

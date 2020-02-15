62406067755f79e4ffd2c60ac7c62f502cc87bce
Lincoln <linkthayer@sbcglobal.net>
2020-02-15 00:50:03 -0500

    adding back the shop checkpoint

---
+++ Scripts/labelPlayerGems.gd
@@ -0,0 +1,10 @@
+extends Label
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	if (GLOBALS.g_current_attacker == "player1"):
+		self.set_text("Defender's Gems: " + str(GLOBALS.g_player2_defender_score))
+	else:
+		self.set_text("Defender's Gems: " + str(GLOBALS.g_player1_defender_score))
